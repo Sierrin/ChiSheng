b2533d14d84e0fdf2bf4d6a14e99ff56d4053c98
魏晓赟 <weixiao12@xdf.cn>
2020-10-21 14:28:32 +0800

    delete all.

--- Example/Pods/Target Support Files/ChiSheng/ChiSheng-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ChiSheng : NSObject
-@end
-@implementation PodsDummy_ChiSheng
-@end
--- Example/Pods/Target Support Files/Pods-ChiSheng_Tests/Pods-ChiSheng_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChiSheng_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ChiSheng_Tests
-@end
